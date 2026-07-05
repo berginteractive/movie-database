395cf02704ed60fda354655ca714b9cd36c0dda8
wicovandenberg <111677006+wicovandenberg@users.noreply.github.com>
2026-07-05 19:10:08 +0200

    homepage latest movies view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,35 @@
+import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, StatusBar, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  StatusBar,
+  Text,
+  View,
+} from "react-native";
 
 const Index = () => {
   const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() =>
+    fetchMovies({
+      query: "",
+    }),
+  );
+
+  console.log("movies", movies);
+
   return (
     <View className="flex-1 bg-primary">
       <StatusBar barStyle="light-content" translucent={true} />
@@ -19,12 +43,41 @@ const Index = () => {
           source={icons.logo}
           className="w-12 h-10 self-center mt-20 mb-5"
         />
-        <View className="flex-1 mt-5">
-          <SearchBar
-            onPress={() => router.push("/search")}
-            placeholder="Search for a movie"
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#AB8BFF"
+            className="mt-10 self-center"
           />
-        </View>
+        ) : moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search for a movie"
+            />
+            <>
+              <Text className="text-white text-lg mb-3 mt-5 font-bold">
+                Latest Movies
+              </Text>
+              <FlatList
+                className="mt-2 pb-32"
+                scrollEnabled={false}
+                data={movies}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10,
+                }}
+                renderItem={({ item }) => <MovieCard {...item} />}
+              />
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
