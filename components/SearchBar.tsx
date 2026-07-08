4b7e0939869bff1d28d57a29a1655e31b351e5c7
wicovandenberg <111677006+wicovandenberg@users.noreply.github.com>
2026-07-08 16:26:29 +0200

    fully implement search and finish search page

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,9 +5,11 @@ import { Image, TextInput, View } from "react-native";
 interface Props {
   onPress?: () => void;
   placeholder: string;
+  value: string;
+  onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({ onPress, placeholder }: Props) => {
+const SearchBar = ({ onPress, placeholder, value, onChangeText }: Props) => {
   return (
     <View className="flex-row items-center bg-dark-100 rounded-full px-5 py-4">
       <Image
@@ -20,7 +22,8 @@ const SearchBar = ({ onPress, placeholder }: Props) => {
         className="flex-1 ml-2 text-white"
         placeholder={placeholder}
         onPress={onPress}
-        onChangeText={() => {}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor="#A8B5DB"
       />
     </View>
